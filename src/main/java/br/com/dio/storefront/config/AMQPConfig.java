76960ba3b8e23f95e90c8a7edbcca21f81697b21
Matheus "Papu" Vincles <120433634+Vincles@users.noreply.github.com>
2026-05-26 15:02:28 -0300

    Update AMQPConfig.java

--- src/main/java/br/com/dio/storefront/config/AMQPConfig.java
+++ src/main/java/br/com/dio/storefront/config/AMQPConfig.java
@@ -1,5 +1,6 @@
 package br.com.dio.storefront.config;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -9,17 +10,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AMQPConfig {
 
+    public static final String ORDERS_EXCHANGE = "storefront.orders.exchange";
+    public static final String ORDERS_QUEUE = "storefront.orders.queue";
+    public static final String ROUTING_KEY = "storefront.orders.routing-key";
+
     @Bean
-    Jackson2JsonMessageConverter jsonMessageConverter() {
+    public DirectExchange ordersExchange() {
+        return new DirectExchange(ORDERS_EXCHANGE);
+    }
+
+    @Bean
+    public Queue ordersQueue() {
+        return QueueBuilder.durable(ORDERS_QUEUE).build();
+    }
+
+    @Bean
+    public Binding bindingOrders(Queue ordersQueue, DirectExchange ordersExchange) {
+        return BindingBuilder.bind(ordersQueue).to(ordersExchange).with(ROUTING_KEY);
+    }
+    
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
-    RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory,
-                                  final Jackson2JsonMessageConverter converter) {
-        var rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(converter);
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
-
 }
